ce4e525f5028c92aa63e63c102b2a7445591b74e
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-09-19 18:15:30 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import IndexPage from "./pages/indexPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<IndexPage />} />
+    </Routes>
+  );
+}
+
+export default App;
