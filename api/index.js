69da7df07641ab07c544e50dde8f590d5b4f483d
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-09-28 00:33:07 +0530

    added new upload by link feature

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const User = require('./models/User');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+
 require('dotenv').config()
 const app = express();
 
@@ -13,6 +15,7 @@ const jwtSecret = 'thisisarandomstring';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -74,5 +77,16 @@ app.get('/profile', (req, res) => {
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
 });
+
+console.log({ __dirname });
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now()+'.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName);
+})
 
 app.listen(4000);
